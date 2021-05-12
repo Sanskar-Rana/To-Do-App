fff75dbee1a36d329731c1010eba02f05a1d6b2d
Sanskar-Rana <rsanskar31@gmail.com>
2021-05-12 23:26:21 +0545

    Database SQLITE

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.todoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.todoapp.Utilis.Databasehelper;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private FloatingActionButton floatingActionButton;
+    private Databasehelper myDB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycleview);
+        floatingActionButton = findViewById(R.id.fab);
+        myDB = new Databasehelper(MainActivity.this);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
